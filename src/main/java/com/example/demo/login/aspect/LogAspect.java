e7e52c588a3cdcf0141ab1c3cfb71a07c3f1b734
Reiya Ono <reiya018018s@gmail.com>
2022-10-18 17:59:41 +0900

    developping dao

--- src/main/java/com/example/demo/login/aspect/LogAspect.java
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -15,6 +15,7 @@ public class LogAspect {
 //	@Before("execution(* *..*.*Controller.*(..))")
 	
 	@Around("execution(* *..*.*Controller.*(..))")
+//	@Around("bean(*Controller)")
 	public Object startLog(ProceedingJoinPoint jp) throws Throwable {
 		
 		System.out.println("メソッド開始: " + jp.getSignature());
@@ -37,4 +38,21 @@ public class LogAspect {
 //	public void endLog(JoinPoint jp) {
 //		System.out.println("メソッド終了：" + jp.getSignature());
 //	}
+	
+	// UserDaoクラスのログ出力
+	@Around("execution(* *..*.*UserDao*.*(..))")
+	public Object daoLog(ProceedingJoinPoint jp) throws Throwable {
+		System.out.println("メソッド開始: " + jp.getSignature());
+		
+		try {
+			Object result = jp.proceed();
+			System.out.println("メソッド終了: " + jp.getSignature());
+			
+			return result;
+		} catch (Exception e) {
+			System.out.println("メソッド異常終了: " + jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
+	}
 }
